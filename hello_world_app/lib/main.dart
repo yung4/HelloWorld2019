542614e52f073b9da8d0e8dafdb2493f82c58004
henrywu00 <wu1461@purdue.edu>
2019-09-14 15:38:28 -0400

    added working checkboxes

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -44,7 +44,7 @@ class _State extends State<myApp>{
         child: Center (
           child: Column(
             children: <Widget>[
-              //Checkbox(value: value1,)
+
               Container(
 
                 child: DrawerHeader(
@@ -66,12 +66,8 @@ class _State extends State<myApp>{
 
 
 
-              ListTile(
-                title: Text('hello'),
-                onTap: (){
-                  Navigator.pop(context);
-                },
-              ),
+
+
               CheckboxListTile(
                 value: value2,
                 onChanged: (bool value) {
@@ -86,6 +82,19 @@ class _State extends State<myApp>{
                 subtitle: Text('我還是不知道'),
                 //activeColor: Colors.red,
               ),
+
+              Align(
+                alignment: Alignment.bottomCenter,
+                child:
+                ListTile(
+                  title: Text('hello'),
+                  onTap: (){
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+
+
             ],
           ),
         ),
