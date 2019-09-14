62479caa5a0c7b39f28901f6a8d04f23b83a9602
yung laptop <yung4@purdue.edu>
2019-09-14 14:40:17 -0400

    wack.

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -3,9 +3,62 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
-      ),
+      title: 'herro world 2019',
+      home: new myApp(),
     ),
   );
+}
+
+//class MyApp extends StatefulWidget {
+//  @override
+//  _State createState() => new _State();
+//}
+
+class myApp extends StatelessWidget {
+
+  bool value1 = false;
+  bool value2 = false;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text('i am confusion'),
+      ),
+      body: Center(
+        child: new Column(
+          children: <Widget>[
+            Text('CHILD 1'),
+            Text('CHILD 2'),
+            Text('CHILD 4'),
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        child: Center (
+          child: Column(
+            children: <Widget>[
+              //Checkbox(value: value1,)
+              DrawerHeader(
+                child: Center(
+                  child: Text('為什麽'),
+                )
+              ),
+              CheckboxListTile(
+                value: value2,
+                onChanged: (bool value){
+
+                },
+                title: Text('我不知道'),
+                subtitle: Text('我還是不知道'),
+                //activeColor: Colors.red,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
